a35929c742c6d2d06b31057833e7ae49b22881e3
Tihh Gonçalves <tihh.gon@gmail.com>
2017-04-25 18:54:37 -0300

    novo hotsite

--- site/index.php
+++ site/index.php
@@ -63,39 +63,39 @@ include('inc.header.php');
     <!-- ################################################################################################ -->
     <ul class="nospace group services">
       <li class="one_third first btmspace-30">
-        <article><a href="#"><i class="fa fa-4x fa-language"></i></a>
-          <h6 class="heading font-x1">Ligula ornare mattis</h6>
-          <p>Mauris vel vehicula lacinia est nunc tincidunt quam sed placerat felis&hellip;</p>
+        <article><a href="#"><i class="fa fa-4x fa-trophy" aria-hidden="true"></i></a>
+          <h6 class="heading font-x1">Mais Profissional</h6>
+          <p>Nossa intensão é ajudar você a crescer profissionalmente, pra ser um dos melhores motoristas profissionais da sua região.</p>
         </article>
       </li>
       <li class="one_third btmspace-30">
-        <article><a href="#"><i class="fa fa-4x fa-balance-scale"></i></a>
-          <h6 class="heading font-x1">Vestibulum at semper</h6>
-          <p>Vitae neque ac ullamcorper nulla id hendrerit phasellus pretium mi in&hellip;</p>
+        <article><a href="#"><i class="fa fa-4x fa-car"></i></a>
+          <h6 class="heading font-x1">Seu carro bem cuidado</h6>
+          <p>Pra cuidar de você é inevitável que seu carro esteja sendo cuidado também. Vamos ajudar você a cuidar dele.</p>
         </article>
       </li>
       <li class="one_third btmspace-30">
-        <article><a href="#"><i class="fa fa-4x fa-map-signs"></i></a>
-          <h6 class="heading font-x1">Tincidunt ullamcorper</h6>
-          <p>Lacinia interdum dui diam pharetra velit eget accumsan turpis neque&hellip;</p>
+        <article><a href="#"><i class="fa fa-4x fa-briefcase"></i></a>
+          <h6 class="heading font-x1">A parte chata é com a gente</h6>
+          <p>Muitos motoristas não gostam de tratar da parte burocrática da profissão. Deixe essa parte com a gente!</p>
         </article>
       </li>
       <li class="one_third first">
-        <article><a href="#"><i class="fa fa-4x fa-binoculars"></i></a>
-          <h6 class="heading font-x1">Dolor nec malesuada</h6>
-          <p>Sagittis massa in metus vehicula blandit tellus pulvinar donec&hellip;</p>
+        <article><a href="#"><i class="fa fa-4x fa-money"></i></a>
+          <h6 class="heading font-x1">Mais lucro pra você</h6>
+          <p>O principal objetivo de ajudar você a organizar sua carreira, é de fazer você lucrar ainda mais! :)</p>
         </article>
       </li>
       <li class="one_third">
-        <article><a href="#"><i class="fa fa-4x fa-paw"></i></a>
-          <h6 class="heading font-x1">Ultrices ut porta sed</h6>
-          <p>Purus elit interdum nibh cursus nisl tellus egestas nunc fusce in&hellip;</p>
+        <article><a href="#"><i class="fa fa-4x fa-futbol-o"></i></a>
+          <h6 class="heading font-x1">Maior qualidade de vida</h6>
+          <p>Com tudo organizado, você ainda terá mais qualidade de vida e tempo com sua família.</p>
         </article>
       </li>
       <li class="one_third">
-        <article><a href="#"><i class="fa fa-4x fa-qrcode"></i></a>
-          <h6 class="heading font-x1">A viverra vestibulum</h6>
-          <p>Id condimentum ligula purus non massa mattis rhoncus cum&hellip;</p>
+        <article><a href="#"><i class="fa fa-4x fa-life-ring"></i></a>
+          <h6 class="heading font-x1">Suporte gratuíto</h6>
+          <p>Nosso sistema é muito fácil, mas você sempre poderá contar com nosso equipe quando precisar.</p>
         </article>
       </li>
     </ul>
