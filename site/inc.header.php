316d2182a8e07438e3794324bf02c9d36f7f6ac2
Tihh Gonçalves <tihh.gon@gmail.com>
2017-04-25 11:41:48 -0300

    novo hotsite

--- site/inc.header.php
+++ site/inc.header.php
@@ -12,6 +12,7 @@ Licence URI: http://www.os-templates.com/template-terms
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
   <link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+  <link href="layout/styles/tihh.css" rel="stylesheet" type="text/css" media="all">
 </head>
 <body id="top">
 <!-- ################################################################################################ -->
