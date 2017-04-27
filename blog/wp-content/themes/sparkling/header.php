846e50397bb93638a101dd13e00a6bf1177c0193
Tihh Gonçalves <tihh.gon@gmail.com>
2017-04-27 18:02:04 -0300

    Blog

--- blog/wp-content/themes/sparkling/header.php
+++ blog/wp-content/themes/sparkling/header.php
@@ -58,7 +58,10 @@ if (isset($_SERVER['HTTP_USER_AGENT']) && (strpos($_SERVER['HTTP_USER_AGENT'], '
 
 							<div id="logo">
 								<?php echo is_home() ?  '<h1 class="site-name">' : '<p class="site-name">'; ?>
-									<a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+									<a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+										<!--<?php bloginfo( 'name' ); ?>-->
+									  <img src="<?= get_template_directory_uri(); ?>/logo.png" id="logo">
+									</a>
 								<?php echo is_home() ?  '</h1>' : '</p>'; ?>
 							</div><!-- end of #logo -->
 
