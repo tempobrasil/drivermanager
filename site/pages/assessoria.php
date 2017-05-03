b59cb642cc00139cbd0c6faf9816b7c591e74123
Tihh Gonçalves <tihh.gon@gmail.com>
2017-05-03 16:36:06 -0300

    up

---
+++ site/pages/assessoria.php
@@ -0,0 +1,64 @@
+<?
+site_setSubTitle('A Assessoria');
+site_setDescription('Conheça todas as vantagens de você ter a ssessoria da DriverUP.');
+site_setTags('benefícios, assessoria, ventagens');
+
+include('inc.header.php');
+?>
+
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <div class="wrapper row2">
+    <div id="breadcrumb" class="hoc clear">
+      <!-- ################################################################################################ -->
+      <ul>
+        <li><a href="<?= GetLink('site/home')?>">Home</a></li>
+        <li><a href="<?= GetLink('site/mensagem')?>">Mensagem</a></li>
+
+      </ul>
+      <!-- ################################################################################################ -->
+    </div>
+  </div>
+
+
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <div class="wrapper row3">
+    <main class="hoc container clear">
+      <!-- main body -->
+      <!-- ################################################################################################ -->
+      <div class="content">
+        <!-- ################################################################################################ -->
+
+        <h1>A Assessoria</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec libero lacus, facilisis sit amet justo sed, dictum dapibus tellus. Suspendisse ultricies convallis ex sed lobortis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras ut eros hendrerit, bibendum elit non, aliquet quam. Pellentesque aliquam mollis nulla id elementum. Integer bibendum arcu mi, at consequat diam volutpat cursus. Vivamus egestas elit nibh, et tincidunt mauris lobortis porta. Sed dui risus, placerat non sagittis quis, molestie id nibh. Phasellus hendrerit nisi a erat consequat, suscipit congue magna ullamcorper.</p>
+        <p>Donec nec mattis arcu. Fusce efficitur tortor lobortis mi aliquet, sed fermentum orci placerat. Nam lacinia laoreet nulla eget placerat. Aenean interdum lacus a lacinia tristique. Phasellus lacinia rhoncus dui, vitae vehicula lacus consectetur at. Proin sem nunc, ornare in risus vitae, imperdiet placerat urna. Vestibulum rhoncus quis mi at mattis. Ut ultricies neque ac placerat vehicula.</p>
+
+
+        <h2>O Aplicativo</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec libero lacus, facilisis sit amet justo sed, dictum dapibus tellus. Suspendisse ultricies convallis ex sed lobortis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras ut eros hendrerit, bibendum elit non, aliquet quam. Pellentesque aliquam mollis nulla id elementum. Integer bibendum arcu mi, at consequat diam volutpat cursus. Vivamus egestas elit nibh, et tincidunt mauris lobortis porta. Sed dui risus, placerat non sagittis quis, molestie id nibh. Phasellus hendrerit nisi a erat consequat, suscipit congue magna ullamcorper.</p>
+        <p>Donec nec mattis arcu. Fusce efficitur tortor lobortis mi aliquet, sed fermentum orci placerat. Nam lacinia laoreet nulla eget placerat. Aenean interdum lacus a lacinia tristique. Phasellus lacinia rhoncus dui, vitae vehicula lacus consectetur at. Proin sem nunc, ornare in risus vitae, imperdiet placerat urna. Vestibulum rhoncus quis mi at mattis. Ut ultricies neque ac placerat vehicula.</p>
+
+
+        <h3>Convênios</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec libero lacus, facilisis sit amet justo sed, dictum dapibus tellus. Suspendisse ultricies convallis ex sed lobortis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras ut eros hendrerit, bibendum elit non, aliquet quam. Pellentesque aliquam mollis nulla id elementum. Integer bibendum arcu mi, at consequat diam volutpat cursus. Vivamus egestas elit nibh, et tincidunt mauris lobortis porta. Sed dui risus, placerat non sagittis quis, molestie id nibh. Phasellus hendrerit nisi a erat consequat, suscipit congue magna ullamcorper.</p>
+        <p>Donec nec mattis arcu. Fusce efficitur tortor lobortis mi aliquet, sed fermentum orci placerat. Nam lacinia laoreet nulla eget placerat. Aenean interdum lacus a lacinia tristique. Phasellus lacinia rhoncus dui, vitae vehicula lacus consectetur at. Proin sem nunc, ornare in risus vitae, imperdiet placerat urna. Vestibulum rhoncus quis mi at mattis. Ut ultricies neque ac placerat vehicula.</p>
+
+
+        <h1>Blog</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec libero lacus, facilisis sit amet justo sed, dictum dapibus tellus. Suspendisse ultricies convallis ex sed lobortis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras ut eros hendrerit, bibendum elit non, aliquet quam. Pellentesque aliquam mollis nulla id elementum. Integer bibendum arcu mi, at consequat diam volutpat cursus. Vivamus egestas elit nibh, et tincidunt mauris lobortis porta. Sed dui risus, placerat non sagittis quis, molestie id nibh. Phasellus hendrerit nisi a erat consequat, suscipit congue magna ullamcorper.</p>
+        <p>Donec nec mattis arcu. Fusce efficitur tortor lobortis mi aliquet, sed fermentum orci placerat. Nam lacinia laoreet nulla eget placerat. Aenean interdum lacus a lacinia tristique. Phasellus lacinia rhoncus dui, vitae vehicula lacus consectetur at. Proin sem nunc, ornare in risus vitae, imperdiet placerat urna. Vestibulum rhoncus quis mi at mattis. Ut ultricies neque ac placerat vehicula.</p>
+
+
+        <!-- ################################################################################################ -->
+      </div>
+      <!-- ################################################################################################ -->
+      <!-- / main body -->
+      <div class="clear"></div>
+    </main>
+  </div>
+<?
+include('inc.footer.php');
+?>
