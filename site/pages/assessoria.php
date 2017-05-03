9bebb5559957707c5231328741ff3f9ef04bc1e6
Tihh Gonçalves <tihh.gon@gmail.com>
2017-05-03 18:26:18 -0300

    up

--- site/pages/assessoria.php
+++ site/pages/assessoria.php
@@ -38,16 +38,19 @@ include('inc.header.php');
 
 
         <h2>Aplicativo</h2>
+        <img src="<?= get_config('SITE_URL')?>site/images/assessoria-app.jpg" class="img-right">
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec libero lacus, facilisis sit amet justo sed, dictum dapibus tellus. Suspendisse ultricies convallis ex sed lobortis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras ut eros hendrerit, bibendum elit non, aliquet quam. Pellentesque aliquam mollis nulla id elementum. Integer bibendum arcu mi, at consequat diam volutpat cursus. Vivamus egestas elit nibh, et tincidunt mauris lobortis porta. Sed dui risus, placerat non sagittis quis, molestie id nibh. Phasellus hendrerit nisi a erat consequat, suscipit congue magna ullamcorper.</p>
         <p>Donec nec mattis arcu. Fusce efficitur tortor lobortis mi aliquet, sed fermentum orci placerat. Nam lacinia laoreet nulla eget placerat. Aenean interdum lacus a lacinia tristique. Phasellus lacinia rhoncus dui, vitae vehicula lacus consectetur at. Proin sem nunc, ornare in risus vitae, imperdiet placerat urna. Vestibulum rhoncus quis mi at mattis. Ut ultricies neque ac placerat vehicula.</p>
 
 
         <h2>Convênios</h2>
+        <img src="<?= get_config('SITE_URL')?>site/images/assessoria-lavacao.jpg" class="img-left">
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec libero lacus, facilisis sit amet justo sed, dictum dapibus tellus. Suspendisse ultricies convallis ex sed lobortis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras ut eros hendrerit, bibendum elit non, aliquet quam. Pellentesque aliquam mollis nulla id elementum. Integer bibendum arcu mi, at consequat diam volutpat cursus. Vivamus egestas elit nibh, et tincidunt mauris lobortis porta. Sed dui risus, placerat non sagittis quis, molestie id nibh. Phasellus hendrerit nisi a erat consequat, suscipit congue magna ullamcorper.</p>
         <p>Donec nec mattis arcu. Fusce efficitur tortor lobortis mi aliquet, sed fermentum orci placerat. Nam lacinia laoreet nulla eget placerat. Aenean interdum lacus a lacinia tristique. Phasellus lacinia rhoncus dui, vitae vehicula lacus consectetur at. Proin sem nunc, ornare in risus vitae, imperdiet placerat urna. Vestibulum rhoncus quis mi at mattis. Ut ultricies neque ac placerat vehicula.</p>
 
 
         <h2>Blog</h2>
+        <img src="<?= get_config('SITE_URL')?>site/images/assessoria-blog.png" class="img-right">
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec libero lacus, facilisis sit amet justo sed, dictum dapibus tellus. Suspendisse ultricies convallis ex sed lobortis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras ut eros hendrerit, bibendum elit non, aliquet quam. Pellentesque aliquam mollis nulla id elementum. Integer bibendum arcu mi, at consequat diam volutpat cursus. Vivamus egestas elit nibh, et tincidunt mauris lobortis porta. Sed dui risus, placerat non sagittis quis, molestie id nibh. Phasellus hendrerit nisi a erat consequat, suscipit congue magna ullamcorper.</p>
         <p>Donec nec mattis arcu. Fusce efficitur tortor lobortis mi aliquet, sed fermentum orci placerat. Nam lacinia laoreet nulla eget placerat. Aenean interdum lacus a lacinia tristique. Phasellus lacinia rhoncus dui, vitae vehicula lacus consectetur at. Proin sem nunc, ornare in risus vitae, imperdiet placerat urna. Vestibulum rhoncus quis mi at mattis. Ut ultricies neque ac placerat vehicula.</p>
 
