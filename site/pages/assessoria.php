fa3953accdf1fed102ba7dd6873bb9357c19622a
Tihh Gonçalves <tihh.gon@gmail.com>
2017-05-03 16:41:44 -0300

    up

--- site/pages/assessoria.php
+++ site/pages/assessoria.php
@@ -37,17 +37,17 @@ include('inc.header.php');
         <p>Donec nec mattis arcu. Fusce efficitur tortor lobortis mi aliquet, sed fermentum orci placerat. Nam lacinia laoreet nulla eget placerat. Aenean interdum lacus a lacinia tristique. Phasellus lacinia rhoncus dui, vitae vehicula lacus consectetur at. Proin sem nunc, ornare in risus vitae, imperdiet placerat urna. Vestibulum rhoncus quis mi at mattis. Ut ultricies neque ac placerat vehicula.</p>
 
 
-        <h2>O Aplicativo</h2>
+        <h2>Aplicativo</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec libero lacus, facilisis sit amet justo sed, dictum dapibus tellus. Suspendisse ultricies convallis ex sed lobortis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras ut eros hendrerit, bibendum elit non, aliquet quam. Pellentesque aliquam mollis nulla id elementum. Integer bibendum arcu mi, at consequat diam volutpat cursus. Vivamus egestas elit nibh, et tincidunt mauris lobortis porta. Sed dui risus, placerat non sagittis quis, molestie id nibh. Phasellus hendrerit nisi a erat consequat, suscipit congue magna ullamcorper.</p>
         <p>Donec nec mattis arcu. Fusce efficitur tortor lobortis mi aliquet, sed fermentum orci placerat. Nam lacinia laoreet nulla eget placerat. Aenean interdum lacus a lacinia tristique. Phasellus lacinia rhoncus dui, vitae vehicula lacus consectetur at. Proin sem nunc, ornare in risus vitae, imperdiet placerat urna. Vestibulum rhoncus quis mi at mattis. Ut ultricies neque ac placerat vehicula.</p>
 
 
-        <h3>Convênios</h3>
+        <h2>Convênios</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec libero lacus, facilisis sit amet justo sed, dictum dapibus tellus. Suspendisse ultricies convallis ex sed lobortis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras ut eros hendrerit, bibendum elit non, aliquet quam. Pellentesque aliquam mollis nulla id elementum. Integer bibendum arcu mi, at consequat diam volutpat cursus. Vivamus egestas elit nibh, et tincidunt mauris lobortis porta. Sed dui risus, placerat non sagittis quis, molestie id nibh. Phasellus hendrerit nisi a erat consequat, suscipit congue magna ullamcorper.</p>
         <p>Donec nec mattis arcu. Fusce efficitur tortor lobortis mi aliquet, sed fermentum orci placerat. Nam lacinia laoreet nulla eget placerat. Aenean interdum lacus a lacinia tristique. Phasellus lacinia rhoncus dui, vitae vehicula lacus consectetur at. Proin sem nunc, ornare in risus vitae, imperdiet placerat urna. Vestibulum rhoncus quis mi at mattis. Ut ultricies neque ac placerat vehicula.</p>
 
 
-        <h1>Blog</h1>
+        <h2>Blog</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec libero lacus, facilisis sit amet justo sed, dictum dapibus tellus. Suspendisse ultricies convallis ex sed lobortis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras ut eros hendrerit, bibendum elit non, aliquet quam. Pellentesque aliquam mollis nulla id elementum. Integer bibendum arcu mi, at consequat diam volutpat cursus. Vivamus egestas elit nibh, et tincidunt mauris lobortis porta. Sed dui risus, placerat non sagittis quis, molestie id nibh. Phasellus hendrerit nisi a erat consequat, suscipit congue magna ullamcorper.</p>
         <p>Donec nec mattis arcu. Fusce efficitur tortor lobortis mi aliquet, sed fermentum orci placerat. Nam lacinia laoreet nulla eget placerat. Aenean interdum lacus a lacinia tristique. Phasellus lacinia rhoncus dui, vitae vehicula lacus consectetur at. Proin sem nunc, ornare in risus vitae, imperdiet placerat urna. Vestibulum rhoncus quis mi at mattis. Ut ultricies neque ac placerat vehicula.</p>
 
