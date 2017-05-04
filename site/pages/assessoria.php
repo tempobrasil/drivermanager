c434f34f347c624eaced6e29e9f591c323b19542
Tiago Gonçalves <Tiago Gonalves>
2017-05-04 00:27:52 -0300

    up

--- site/pages/assessoria.php
+++ site/pages/assessoria.php
@@ -33,26 +33,39 @@ include('inc.header.php');
         <!-- ################################################################################################ -->
 
         <h1>A Assessoria</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec libero lacus, facilisis sit amet justo sed, dictum dapibus tellus. Suspendisse ultricies convallis ex sed lobortis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras ut eros hendrerit, bibendum elit non, aliquet quam. Pellentesque aliquam mollis nulla id elementum. Integer bibendum arcu mi, at consequat diam volutpat cursus. Vivamus egestas elit nibh, et tincidunt mauris lobortis porta. Sed dui risus, placerat non sagittis quis, molestie id nibh. Phasellus hendrerit nisi a erat consequat, suscipit congue magna ullamcorper.</p>
-        <p>Donec nec mattis arcu. Fusce efficitur tortor lobortis mi aliquet, sed fermentum orci placerat. Nam lacinia laoreet nulla eget placerat. Aenean interdum lacus a lacinia tristique. Phasellus lacinia rhoncus dui, vitae vehicula lacus consectetur at. Proin sem nunc, ornare in risus vitae, imperdiet placerat urna. Vestibulum rhoncus quis mi at mattis. Ut ultricies neque ac placerat vehicula.</p>
+        <p>Mais do que um aplicativo ou convênio, mas uma verdadeira assessoria profissional. Isso mesmo. É assim que queremos ser lembrados por você, como seu melhor parceiro. A Driver UP é uma Assessoria para Motoristas Profissionais. Em outras palavras, a gente se dedica pra contribuir para que você tenha uma grande uma carreira profissional e isso tanto pra você que já é motorista e quer crescer ainda mais, quanto pra você que está pensando em iniciar agora com esse trabalho.</p>
+          <p>Nosso ponto forte é o relacionamento. Além de oferecermos uma série de ferramentas pra você e ajudar no seu dia a dia como motorista profissional, fazemos questão de estabelecer uma parceria a nível pessoal com nossos assessorados. Isso significa que toda nossa equipe está a sua disposição, pronto para ajudar você no que for preciso.</p>
+          <p>Confira abaixo as principais vantagens de ser um assessorado Driver UP. Conte com a gente e entre pro time ainda hoje!</p>
 
+        <div class="clearboth"></div>
 
         <h2>Aplicativo</h2>
         <img src="<?= get_config('SITE_URL')?>site/images/assessoria-app.jpg" class="img-right">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec libero lacus, facilisis sit amet justo sed, dictum dapibus tellus. Suspendisse ultricies convallis ex sed lobortis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras ut eros hendrerit, bibendum elit non, aliquet quam. Pellentesque aliquam mollis nulla id elementum. Integer bibendum arcu mi, at consequat diam volutpat cursus. Vivamus egestas elit nibh, et tincidunt mauris lobortis porta. Sed dui risus, placerat non sagittis quis, molestie id nibh. Phasellus hendrerit nisi a erat consequat, suscipit congue magna ullamcorper.</p>
-        <p>Donec nec mattis arcu. Fusce efficitur tortor lobortis mi aliquet, sed fermentum orci placerat. Nam lacinia laoreet nulla eget placerat. Aenean interdum lacus a lacinia tristique. Phasellus lacinia rhoncus dui, vitae vehicula lacus consectetur at. Proin sem nunc, ornare in risus vitae, imperdiet placerat urna. Vestibulum rhoncus quis mi at mattis. Ut ultricies neque ac placerat vehicula.</p>
+        <p>Pra nós, você  - Motorista Profissional - é um empresário. Até mesmo você que trabalha como renda extra, em horários esporádicos, nós olhamos pra você como alguém dono do próprio negócio. Porém sabemos que a maioria que está nessa carreira entende mesmo é de dirigir e a parte administrativa considera muito complicada. É aí que nosso aplicativo ajuda você! Nosso sistema cuida não só do dinheiro que você ganha, mas te ajuda a cuidar melhor do seu veículo e todos os investimentos que faz nele.</p>
+        <p>Vamos ajudar você nessa jornada como empresário. Ajudaremos você a entender qual é seu real lucro, seus gastos com combustível, manutenção do carro, documentação, lavação, seguro, entre todas as inúmeras outras provisões do seu carro. Com nosso sistema, você será somente um motorista qualquer, mas entenderá e virá sua vida profissional – com empresa, dono do próprio negócio – crescer cada vez mais, dando ainda maior estabilidade profissional e financeira.</p>
 
+        <div class="clearboth"></div>
 
         <h2>Convênios</h2>
         <img src="<?= get_config('SITE_URL')?>site/images/assessoria-lavacao.jpg" class="img-left">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec libero lacus, facilisis sit amet justo sed, dictum dapibus tellus. Suspendisse ultricies convallis ex sed lobortis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras ut eros hendrerit, bibendum elit non, aliquet quam. Pellentesque aliquam mollis nulla id elementum. Integer bibendum arcu mi, at consequat diam volutpat cursus. Vivamus egestas elit nibh, et tincidunt mauris lobortis porta. Sed dui risus, placerat non sagittis quis, molestie id nibh. Phasellus hendrerit nisi a erat consequat, suscipit congue magna ullamcorper.</p>
-        <p>Donec nec mattis arcu. Fusce efficitur tortor lobortis mi aliquet, sed fermentum orci placerat. Nam lacinia laoreet nulla eget placerat. Aenean interdum lacus a lacinia tristique. Phasellus lacinia rhoncus dui, vitae vehicula lacus consectetur at. Proin sem nunc, ornare in risus vitae, imperdiet placerat urna. Vestibulum rhoncus quis mi at mattis. Ut ultricies neque ac placerat vehicula.</p>
+        <p>Ter uma carreira como Motorista Profissional de destaque é algo que exige alguns cuidados que você deve seguir à risca. Desde ter seu carro sempre limpo e cheiroso, até ter um bom seguro e um bom mecânico a disposição quando você precisar. Nós, da Driver UP, estamos fechando parcerias pelo Brasil inteiro para manter a sua disposição diversos serviços com preços diferenciados e atendimento especial. </p>
+        <p>Estamos empenhados a dar a você toda assistência que você precisa pra ser um dos melhores profissionais da sua região. Queremos ajudar você a cuidar e investir no seu carro, que é agora seu instrumento de trabalho. Queremos ser a assessoria que você pode contar diante de toda e qualquer situação que na sua carreira.</p>
 
+        <div class="clearboth"></div>
 
         <h2>Blog</h2>
-        <img src="<?= get_config('SITE_URL')?>site/images/assessoria-blog.png" class="img-right">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec libero lacus, facilisis sit amet justo sed, dictum dapibus tellus. Suspendisse ultricies convallis ex sed lobortis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras ut eros hendrerit, bibendum elit non, aliquet quam. Pellentesque aliquam mollis nulla id elementum. Integer bibendum arcu mi, at consequat diam volutpat cursus. Vivamus egestas elit nibh, et tincidunt mauris lobortis porta. Sed dui risus, placerat non sagittis quis, molestie id nibh. Phasellus hendrerit nisi a erat consequat, suscipit congue magna ullamcorper.</p>
-        <p>Donec nec mattis arcu. Fusce efficitur tortor lobortis mi aliquet, sed fermentum orci placerat. Nam lacinia laoreet nulla eget placerat. Aenean interdum lacus a lacinia tristique. Phasellus lacinia rhoncus dui, vitae vehicula lacus consectetur at. Proin sem nunc, ornare in risus vitae, imperdiet placerat urna. Vestibulum rhoncus quis mi at mattis. Ut ultricies neque ac placerat vehicula.</p>
+        <img src="<?= get_config('SITE_URL')?>site/images/assessoria-blog.png" class="img-right" style="position: relative; margin-top: -50px;">
+        <p>Temos um ambiente de blog exclusivo para nossos motoristas assessorados. Lá informamos desde dicas que ajudam você a oferecer um serviço ainda mais qualificado para seus passageiros, até toda informação a respeito de tudo o que acontece no mundo dos aplicativos de transporte. Toda atualização, você fica sabendo detalhadamente o que muda pra você.</p>
+        <p>E temos tudo muito organizado pra você encontrar com facilidade o que procura. Categorizado por região, aplicativo, assuntos, a navegação do nosso blog é super fácil e intuitiva, contribuindo muito pra que você esteja sempre bem informado.
+        </p>
+
+        <div class="clearboth"></div>
+
+        <h2>Outros Serviços</h2>
+
+        <p>Bom, como se já não fosse suficiente tudo o que oferecemos a você, dispomos ainda de alguns serviços específicos que agregarão ainda mais na sua carreira. Desde cartões de visita personalizados de qualidade, até alguns produtos especiais para motoristas profissionais usarem no seu carro.</p>
+        <p>Definitivamente, nosso negócio é fazer o seu dar certo. Queremos de todas as formas ajudar você a crescer profissionalmente. Hoje, esses são os principais serviços que oferecemos aos nossos motoristas assessorados, mas estamos trabalhando todos os dias para oferecer ainda mais benefícios e ajudas à você, além de melhor a qualidade do que já oferecemos.</p>
+        <p>Somos a Driver UP e queremos <i>dar um up</i> na sua carreira como motorista profissional, conte com a gente!</p>
 
 
         <!-- ################################################################################################ -->
